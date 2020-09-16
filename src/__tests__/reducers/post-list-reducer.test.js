c845e8c6235cc533f090d5b5ea97daa8b79facfa
Allison Sadin <aesadin@gmail.com>
2020-09-16 15:13:41 -0700

    fail second test
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>
    Co-authored-by: James Henager <jameschenager@gmail.com>
    Co-authored-by: meganhepner <megan.hepner@gmail.com>

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -2,6 +2,37 @@ import postListReducer from '../../reducers/post-list-reducer'
 
 describe('postListReducer', () => {
 
+    let action;
+    const postData = {
+      title: 'Pokemon',
+      author: 'James',
+      body: 'Pikachu',
+      date: '4/21/1992',
+      id: 1
+    };
+
+    test('Should successfully add new post data to masterPostList', () => {
+      const { title, author, body, date, id } = postData;
+      action = {
+        type: 'ADD_POST',
+        title: title,
+        author: author,
+        body: body,
+        date: date,
+        id: id
+      };
+
+      expect(postListReducer({}, action)).toEqual({
+        [id] : {
+          title: title,
+          author: author,
+          body: body,
+          date: date,
+          id: id
+        }
+      });
+    });
+
     test('Should return default state if there is no action type passed into the reducer', () => {
       expect(postListReducer({}, { type: null })).toEqual({});
   });
